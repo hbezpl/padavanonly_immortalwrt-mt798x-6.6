9cabf28c33e45ae42947f754d5f6a48a17150bd6
hbezpl <hbezpl@gmail.com>
2026-01-10 09:30:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -102,3 +102,7 @@ git branch --set-upstream-to=origin/master master
 mv luci-app-openclash ../
 cd ../../
 rm -rf package/openclash
+
+# 修正不能web升级报错问题
+sed -i 's/rax3000m-emmc/rax3000m-emmc-mtk/g' target/linux/mediatek/dts/mt7981b-cmcc-rax3000m-emmc-mtk.dts
+sed -i 's/rax3000m-nand/rax3000m-nand-mtk/g' target/linux/mediatek/dts/mt7981b-cmcc-rax3000m-nand-mtk.dts
