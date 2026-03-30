6b83c97d21a0e235d6bee2f91833ca1b1503d843
hbezpl <hbezpl@gmail.com>
2026-03-30 16:25:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -123,6 +123,7 @@ define Device/h3c_magic-nx30-pro-nmbm-112m\
   DEVICE_DTS := mt7981b-h3c-magic-nx30-pro-nmbm-112m\
   DEVICE_DTS_DIR := ../dts\
   DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware\
+  SUPPORTED_DEVICES += h3c,magic-nx30-pro-nmbm \
   UBINIZE_OPTS := -E 5\
   BLOCKSIZE := 128k\
   PAGESIZE := 2048\
