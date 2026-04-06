9d56c359905bd23116309f774858a54ffa6cfa44
hbezpl <hbezpl@gmail.com>
2026-04-06 12:23:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -104,10 +104,6 @@ mv luci-app-openclash ../
 cd ../../
 rm -rf package/openclash
 
-# 修正不能web升级报错问题
-#sed -i 's/rax3000m-emmc/rax3000m-emmc-mtk/g' target/linux/mediatek/dts/mt7981b-cmcc-rax3000m-emmc-mtk.dts
-#sed -i 's/rax3000m-nand/rax3000m-nand-mtk/g' target/linux/mediatek/dts/mt7981b-cmcc-rax3000m-nand-mtk.dts
-
 #添加上网时间控制
 git clone https://github.com/gaobin89/luci-app-timecontrol package/luci-app-timecontrol
 sed -i '/$(eval $(call BuildPackage,$(PKG_NAME)))/s/^/#/' package/luci-app-timecontrol/luci-app-timecontrol/Makefile
